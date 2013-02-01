065ebfa92f2174bd8feae612b50fc7fec2029fe9
Slava Vishnyakov <bomboze+1@gmail.com>
2013-02-01 20:49:39 +0400

    Update ubuntu-install-passenger.sh

--- ubuntu-install-passenger.sh
+++ ubuntu-install-passenger.sh
@@ -7,4 +7,5 @@ fi
 
 sudo mkdir /opt/nginx; sudo chown `whoami` /opt/nginx
 passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx
-
+sudo wget https://raw.github.com/slava-vishnyakov/useful-stuff/master/init.d-nginx.conf -O /etc/init.d/nginx
+sudo update-rc.d nginx defaults
