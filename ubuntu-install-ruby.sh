5177f93a91517a2d30a52c34658ffea7e53ff220
Slava Vishnyakov <bomboze+1@gmail.com>
2013-02-01 20:12:34 +0400

    Update ubuntu-install-ruby.sh

--- ubuntu-install-ruby.sh
+++ ubuntu-install-ruby.sh
@@ -1,3 +1,8 @@
+if [ -d ~/.rvm ]; then
+   echo "RVM already installed"
+   exit 1;
+fi
+
 sudo apt-get -y update
 sudo apt-get -y install curl libcurl4-gnutls-dev git nodejs build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
 \curl -L https://get.rvm.io | bash -s stable --ruby
