211c62846942ec211fe77612c5a09800f9b8ce54
Slava Vishnyakov <bomboze+1@gmail.com>
2013-02-01 19:46:38 +0400

    Update ubuntu-install-google-dns-servers.sh

--- ubuntu-install-google-dns-servers.sh
+++ ubuntu-install-google-dns-servers.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "  nameserver 8.8.8.8" | sudo tee /etc/resolvconf/resolv.conf.d/tail
-echo "  nameserver 8.8.4.4" | sudo tee /etc/resolvconf/resolv.conf.d/tail
+echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" | sudo tee -a /etc/resolvconf/resolv.conf.d/head;
 sudo /etc/init.d/resolvconf restart
