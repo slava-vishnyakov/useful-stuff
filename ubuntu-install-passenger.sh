7fa5bec03f17391d30a8d21bc7662aa21a537776
Slava Vishnyakov <bomboze+1@gmail.com>
2013-02-01 21:35:08 +0400

    Update ubuntu-install-passenger.sh

--- ubuntu-install-passenger.sh
+++ ubuntu-install-passenger.sh
@@ -5,12 +5,20 @@ if [ -d /opt/nginx ]; then
    exit 1;
 fi
 
+
 gem install passenger
+
 sudo mkdir /opt/nginx; sudo chown `whoami` /opt/nginx
 passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx
+
 sudo wget https://raw.github.com/slava-vishnyakov/useful-stuff/master/init.d-nginx.conf -O /etc/init.d/nginx
+sudo chmod o+x /etc/init.d/nginx
+
 sudo update-rc.d nginx defaults
+
 sudo wget https://raw.github.com/slava-vishnyakov/useful-stuff/master/nginx.conf -O /opt/nginx/conf/nginx.conf
-sudo mkdir /opt/nginx/conf/rails-sites;
+sudo mkdir /opt/nginx/conf/rails-sites
+
+sudo useradd -s /bin/bash -d /home/rails -m rails
 
 sudo service nginx start
