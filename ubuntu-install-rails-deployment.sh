624c7ea5790a76bf015ebf7be310e59c0fa03685
Slava Vishnyakov <bomboze+1@gmail.com>
2013-02-02 17:34:26 +0400

    Update ubuntu-install-rails-deployment.sh

--- ubuntu-install-rails-deployment.sh
+++ ubuntu-install-rails-deployment.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
 if id rails > /dev/null 2>&1
+then
   echo "User rails exists. OK"
 else
   echo "Creating user rails..."
   sudo useradd -s /bin/bash -d /home/rails -m rails
 fi
+
 
 echo "Installing RVM to user 'rails'"
 
