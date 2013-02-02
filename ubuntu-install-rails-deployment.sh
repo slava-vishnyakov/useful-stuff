42b25712858f3fd103d4c99ef1ea18117505a4de
Slava Vishnyakov <bomboze+1@gmail.com>
2013-02-02 18:08:01 +0400

    Update ubuntu-install-rails-deployment.sh

--- ubuntu-install-rails-deployment.sh
+++ ubuntu-install-rails-deployment.sh
@@ -11,7 +11,7 @@ fi
 echo "rails ALL=(ALL) NOPASSWD: /usr/bin/apt-get, /bin/mkdir, /bin/chown, /bin/chmod, /usr/sbin/update-rc.d, /usr/bin/service" | sudo tee -a /etc/sudoers
 
 echo "Installing RVM to user 'rails'"
-curl -L https://raw.github.com/slava-vishnyakov/useful-stuff/master/ubuntu-install-ruby.sh | sudo -H -u rails bash -e
+curl -L https://raw.github.com/slava-vishnyakov/useful-stuff/master/ubuntu-install-ruby.sh | sudo -H -u rails bash -l -e
 
 echo "Installing Passenger to /opt/nginx/"
-curl -L https://raw.github.com/slava-vishnyakov/useful-stuff/master/ubuntu-install-passenger.sh | sudo -H -u rails bash -e
+curl -L https://raw.github.com/slava-vishnyakov/useful-stuff/master/ubuntu-install-passenger.sh | sudo -H -u rails bash -l -e
