ae20f008bad2343ff05dd1061618bef27f221708
Slava Vishnyakov <bomboze+1@gmail.com>
2013-02-02 18:57:23 +0400

    Update ubuntu-install-rails-deployment.sh

--- ubuntu-install-rails-deployment.sh
+++ ubuntu-install-rails-deployment.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -e
 
-if id rails > /dev/null 2>&1
-then
-  echo "User rails exists. OK"
-else
-  echo "Creating user rails..."
-  sudo useradd -s /bin/bash -d /home/rails -m rails
-fi
+id rails1 || (echo "Creating user rails..."; sudo useradd -s /bin/bash -d /home/rails -m rails)
 
 echo "rails ALL=(ALL) NOPASSWD: /usr/bin/wget, /usr/bin/curl, /usr/bin/apt-get, /bin/mkdir, /bin/chown, /bin/chmod, /usr/sbin/update-rc.d, /usr/bin/service" | sudo tee -a /etc/sudoers
 
