14c1393b64cc8c461d24c639670d47c51ffdf72b
Slava Vishnyakov <bomboze+1@gmail.com>
2013-02-02 18:49:29 +0400

    Update ubuntu-install-passenger.sh

--- ubuntu-install-passenger.sh
+++ ubuntu-install-passenger.sh
@@ -24,7 +24,18 @@ echo "update-rc.d"
 sudo update-rc.d nginx defaults
 
 echo "Installing nginx.conf"
-sudo wget https://raw.github.com/slava-vishnyakov/useful-stuff/master/nginx.conf -O /opt/nginx/conf/nginx.conf
+sudo wget https://raw.github.com/slava-vishnyakov/useful-stuff/master/nginx.conf -O /opt/nginx/conf/nginx.conf-new
+
+echo "Replacing passenger_ruby and passenger_root with actual Passenger data"
+ruby -e "
+  orig_file = IO.read '/opt/nginx/conf/nginx.conf';
+  new_file = IO.read '/opt/nginx/conf/nginx.conf-new';
+  new_file.sub! /passenger_ruby (.*?);/, orig_file.match(/passenger_ruby (.*?);/)[0];
+  new_file.sub! /passenger_root (.*?);/, orig_file.match(/passenger_root (.*?);/)[0];
+  IO.write '/opt/nginx/conf/nginx.conf', new_file
+  IO.write '/opt/nginx/conf/nginx.conf-orig', old_file
+  File.unlink '/opt/nginx/conf/nginx.conf-new'
+"
 
 echo "Creating /opt/nginx/conf/rails-sites"
 sudo mkdir /opt/nginx/conf/rails-sites
