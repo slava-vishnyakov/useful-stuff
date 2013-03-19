f6b8193348b89d1f683d70ded03a9bae07e8ba30
Slava Vishnyakov <bomboze+1@gmail.com>
2013-03-19 17:28:37 +0100

    Update ubuntu-install-rails-deployment.sh

--- ubuntu-install-rails-deployment.sh
+++ ubuntu-install-rails-deployment.sh
@@ -9,3 +9,25 @@ curl -L https://raw.github.com/slava-vishnyakov/useful-stuff/master/ubuntu-insta
 
 echo "Installing Passenger to /opt/nginx/"
 curl -L https://raw.github.com/slava-vishnyakov/useful-stuff/master/ubuntu-install-passenger.sh | sudo -H -u rails bash -l -e
+
+sudo -u rails /bin/bash -lc "mkdir /home/rails/.ssh; chmod 0700 /home/rails/.ssh; touch /home/rails/.ssh/authorized_keys"
+
+echo "Run this: "
+echo ""
+echo "rails$ sudo vi /home/rails/.ssh/authorized_keys"
+echo ""
+echo "to add the ssh key to 'rails' user. Then you can ssh as rails@"
+echo ""
+echo "Create a file like /opt/nginx/conf/rails-sites/test.com.conf"
+echo ""
+echo "server {"
+echo "  listen 80;"
+echo "  server_name test.com;"
+echo "  passenger_on;"
+echo "  passenger_root /home/rails/applications/test.com/current/public;"
+echo "  # passenger_user nobody;"
+echo "  passenger_max_requests 500;"
+echo "}"
+echo ""
+echo "Then run this to reload: "
+echo "rails$ sudo /opt/nginx/sbin/nginx -t && sudo /opt/nginx/sbin/nginx -s reload"
