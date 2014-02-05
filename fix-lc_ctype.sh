0e27adb13496dc3b61f3d86decf913f9753adbf0
Slava Vishnyakov <bomboze@gmail.com>
2014-02-05 22:14:47 +0100

    Update fix-lc_ctype.sh

--- fix-lc_ctype.sh
+++ fix-lc_ctype.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "LC_ALL=en_US.UTF-8" >> /etc/environment
-echo "LANG=en_US.UTF-8" >> /etc/environment
+echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/environment
+echo "LANG=en_US.UTF-8" | sudo tee -a /etc/environment
 sudo locale-gen en_US en_US.UTF-8
 sudo dpkg-reconfigure locales
